6032bb40fc36f72d056e45475d0bd8cd6192b55d
MRTZA <murtaza.hakimi@gmail.com>
2017-03-30 11:29:40 -0500

    GUI Prep
    Added files for the GUI

---
+++ main.cpp
@@ -0,0 +1,22 @@
+
+#include "std_lib_facilities_4.h"
+
+int main(){
+
+  // menu options
+  cout << "Options: " << endl
+  << "1. Display Brokerage State" << endl
+  << "2. New Patron" << endl
+  << "3. Existing Patron" << endl
+  << "4. Patron Info" << endl
+  << "5. Add Cash" << endl
+  << "6. Remove Cash" << endl
+  << "7. Stock Sale" << endl
+  << "8. Stock Buy" << endl
+  << "9. Overdrawn Patrons" << endl
+  << "10. Public Transaction History" << endl
+  << "11. Quit" << endl;
+
+  return 0;
+
+}
