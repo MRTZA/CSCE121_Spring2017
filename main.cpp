b8b7eefbe507a1a55ecb362af68ab892524340d0
MRTZA <murtaza.hakimi@gmail.com>
2017-04-02 16:52:33 -0500

    GUI stuff

--- main.cpp
+++ main.cpp
@@ -1,5 +1,47 @@
 
 #include "std_lib_facilities_4.h"
+#include "Graph.h"
+#include "GUI.h"
+#include "Window.h"
+#include <iostream>    // for i/o
+#include <sstream>
+
+using namespace Graph_lib;
+using namespace std;
+
+// -----------------------------------------------
+// define a struct that is a window in which the
+// menu options can be displayed via a GUI
+
+struct Brokerage_window : Graph_lib::Window { // inherits from Window
+
+  // constructor
+  Brokerage_window(Point xy,    // top lefthand corner
+    int w,                      // width
+    int h,                      // height
+    const string& title);       // label
+
+private:
+
+  // widgets:
+  Menu options_menu;                   // menu of main options
+  Button menu_button;                  // button to display the option menu
+
+  // function members
+
+  void hide_menu() {
+    // hides the options menu and shows the button to display the menu
+    options_menu.hide();
+    menu_button.show();
+  }
+
+  void BrokerageState_pressed() {
+    //write in Graph.cpp & Graph.h??
+    hide_menu();
+  }
+
+
+}
 
 int main(){
 
